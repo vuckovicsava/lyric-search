0716e18f0d9e273a7c3891f73cb3f7fa104b28a8
Sava <sava.vuckovic@quantox.com>
2019-01-23 13:45:51 +0100

    listing out top 10 tracks using Track component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from './context';
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <>
-          <Navbar/>
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Index} />
-            </Switch>
-          </div>
-        </>
-      </BrowserRouter>
+      <Provider>
+        <BrowserRouter>
+          <>
+            <Navbar/>
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index} />
+              </Switch>
+            </div>
+          </>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Consumer } from '../../context';
+import Track from './Track';
 
 export default class Tracks extends Component {
   render() {
     return (
-      <div>
-        Tracks
-      </div>
+      <Consumer>
+        {({ track_list, heading }) => {
+          if (track_list === undefined || track_list.length === 0) {
+            return <h3>Loading...</h3>;
+          } else {
+            return (
+              <>
+                <h3 className="text-center mb-4">{heading}</h3>
+                <div className="row">
+                  {track_list.map(item => (
+                    <Track track={item.track} key={item.track.track_id} />
+                  ))}
+                </div>
+              </>
+            );
+          }
+        }}
+      </Consumer>
     )
   }
 }
